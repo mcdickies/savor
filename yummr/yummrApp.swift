64a20485ad5ebe3873bd3be5ec9a9563a4b6a8c8
Jakub Wozny <jwozn2@illinois.edu>
2025-06-27 21:07:22 -0500

    Initial Commit

---
+++ yummr/yummrApp.swift
@@ -0,0 +1,17 @@
+//
+//  yummrApp.swift
+//  yummr
+//
+//  Created by kuba woahz on 6/27/25.
+//
+
+import SwiftUI
+
+@main
+struct yummrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
