95c50fdc48327e78d396b49e5dce961283b1f7d5
Jakub Wozny <jwozn2@illinois.edu>
2025-09-18 20:39:13 -0500

    ok newest working version

--- yummr/AppUser.swift
+++ yummr/AppUser.swift
@@ -1,5 +1,14 @@
+//
+//  AppUser 2.swift
+//  yummr
+//
+//  Created by kuba woahz on 9/16/25.
+//
+
+
 import Foundation
 import FirebaseFirestore
+
 
 struct AppUser: Identifiable, Codable {
     @DocumentID var id: String?
@@ -24,52 +33,3 @@ struct AppUser: Identifiable, Codable {
         case topFoods
         case healthMetrics
     }
-
-    init(id: String? = nil,
-         handle: String = "",
-         displayName: String = "",
-         profileImageURL: String? = nil,
-         bannerImageURL: String? = nil,
-         bio: String? = nil,
-         followerCount: Int? = nil,
-         followingCount: Int? = nil,
-         topFoods: [String]? = nil,
-         healthMetrics: [String: String]? = nil) {
-        self.id = id
-        self.handle = handle
-        self.displayName = displayName
-        self.profileImageURL = profileImageURL
-        self.bannerImageURL = bannerImageURL
-        self.bio = bio
-        self.followerCount = followerCount
-        self.followingCount = followingCount
-        self.topFoods = topFoods
-        self.healthMetrics = healthMetrics
-    }
-
-    init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-        self.handle = try container.decodeIfPresent(String.self, forKey: .handle) ?? ""
-        self.displayName = try container.decodeIfPresent(String.self, forKey: .displayName) ?? ""
-        self.profileImageURL = try container.decodeIfPresent(String.self, forKey: .profileImageURL)
-        self.bannerImageURL = try container.decodeIfPresent(String.self, forKey: .bannerImageURL)
-        self.bio = try container.decodeIfPresent(String.self, forKey: .bio)
-        self.followerCount = try container.decodeIfPresent(Int.self, forKey: .followerCount)
-        self.followingCount = try container.decodeIfPresent(Int.self, forKey: .followingCount)
-        self.topFoods = try container.decodeIfPresent([String].self, forKey: .topFoods)
-        self.healthMetrics = try container.decodeIfPresent([String: String].self, forKey: .healthMetrics)
-    }
-
-    func encode(to encoder: Encoder) throws {
-        var container = encoder.container(keyedBy: CodingKeys.self)
-        try container.encode(handle, forKey: .handle)
-        try container.encode(displayName, forKey: .displayName)
-        try container.encodeIfPresent(profileImageURL, forKey: .profileImageURL)
-        try container.encodeIfPresent(bannerImageURL, forKey: .bannerImageURL)
-        try container.encodeIfPresent(bio, forKey: .bio)
-        try container.encodeIfPresent(followerCount, forKey: .followerCount)
-        try container.encodeIfPresent(followingCount, forKey: .followingCount)
-        try container.encodeIfPresent(topFoods, forKey: .topFoods)
-        try container.encodeIfPresent(healthMetrics, forKey: .healthMetrics)
-    }
-}
